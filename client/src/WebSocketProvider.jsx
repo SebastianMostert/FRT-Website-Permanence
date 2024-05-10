a8b3cb14f84d52e010df41b1f3e95884a251177a
Sebastian <sebastianmostert663@gmail.com>
2024-05-10 20:36:18 +0200

    Update WebSocketProvider.jsx

--- client/src/WebSocketProvider.jsx
+++ client/src/WebSocketProvider.jsx
@@ -10,11 +10,15 @@ const WebSocketProvider = ({ children }) => {
     useEffect(() => {
         const domain = window.location.hostname;
         const protocol = window.location.protocol === 'https:' ? 'wss' : 'ws';
+        let hasPort = false;
+        if (domain === 'localhost') {
+            hasPort = true;
+        }
+
         const port = 3000;
-        const newSocket = new WebSocket(`${protocol}://${domain}:${port}`); // Replace with your WebSocket server URL
+        const newSocket = new WebSocket(`${protocol}://${domain}${hasPort ? `:${port}` : ''}`); // Replace with your WebSocket server URL
 
         // Event listeners and message handling here
-
         setSocket(newSocket);
 
         return () => {
